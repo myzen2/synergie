07840b9f06842a2ceef11343677db3760b2214d2
myzen2 <mehdi.zenbil@orange.fr>
2016-04-04 21:01:33 +0200

    fix trashing error

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,9 +4,6 @@ class ConversationsController < ApplicationController
   before_action :get_conversation, except: [:index, :empty_trash]
   before_action :get_box, only: [:index]
 
-  def show
-  end
-
   def index
     @conversations = if @box.eql? 'inbox'
                        @mailbox.inbox
@@ -14,13 +11,23 @@ class ConversationsController < ApplicationController
                        @mailbox.sentbox
                      else
                        @mailbox.trash
-    end
-    @conversations = @mailbox.inbox.paginate(page: params[:page], per_page: 10)
+                     end
+
+    @conversations = @conversations.paginate(page: params[:page], per_page: 10)
+  end
+
+  def show
+  end
+
+  def mark_as_read
+    @conversation.mark_as_read(current_user)
+    flash[:success] = 'The conversation was marked as read.'
+    redirect_to conversations_path
   end
 
   def reply
     current_user.reply_to_conversation(@conversation, params[:body])
-    flash[:success] = 'Réponse envoyée'
+    flash[:success] = 'Reply sent'
     redirect_to conversation_path(@conversation)
   end
 
@@ -40,13 +47,7 @@ class ConversationsController < ApplicationController
     @mailbox.trash.each do |conversation|
       conversation.receipts_for(current_user).update_all(deleted: true)
     end
-    flash[:success] = 'Votre corbeille a été vidée !'
-    redirect_to conversations_path
-  end
-
-  def mark_as_read
-    @conversations.mark_as_read(current_user)
-    flash[:success] = 'La conversation a été marquée comme lue'
+    flash[:success] = 'Your trash was cleaned!'
     redirect_to conversations_path
   end
 
@@ -56,14 +57,14 @@ class ConversationsController < ApplicationController
     @mailbox ||= current_user.mailbox
   end
 
+  def get_conversation
+    @conversation ||= @mailbox.conversations.find(params[:id])
+  end
+
   def get_box
     if params[:box].blank? || !%w(inbox sent trash).include?(params[:box])
       params[:box] = 'inbox'
     end
     @box = params[:box]
-  end
-
-  def get_conversation
-    @conversation ||= @mailbox.conversations.find(params[:id])
   end
 end
